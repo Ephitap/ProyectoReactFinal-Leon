3b905bb81208a890d8da8a1ffee1726006a3b6be
abraham leon <abraham.leon1150@gmail.com>
2023-03-11 21:43:45 -0300

    PreEntrega2 Leon

--- src/components/ItemDetailContainer/index.js
+++ src/components/ItemDetailContainer/index.js
@@ -1,30 +1,53 @@
-import './styles.scss';
-import React, {useState, useEffect} from 'react';
-function ItemDetailContainer({greeting}) {
+import "./styles.scss";
+import { useState, useEffect } from "react";
+import products from "../../products/products";
+import { useParams } from "react-router-dom";
 
-  
-    const [user, setUsers] = useState([]);
-  
-    useEffect(() => {
-      fetch("https://reqres.in/api/users")
-      .then(res => {
+function ItemDetailContainer({ greeting }) {
+  const [user, setUser] = useState({});
+
+  const params = useParams();
+  const idUser = params.idUser;
+
+  /*   useEffect(() => {
+    fetch("https://reqres.in/api/users/" + idUser)
+      .then((res) => {
         return res.json();
       })
-      .then(json => {
-        console.log('json', json);
-        setUsers(json.data);
+      .then((json) => {
+        console.log("respuesta", json);
+        setUser(json.data);
       });
-    }, []);
-  
+  }, []); */
+
+  useEffect(() => {
+    const promesaItem = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let encontrado = products.find((item) => item.id === Number(idUser));
+        resolve(encontrado);
+      }, 2000);
+    });
+
+    promesaItem.then((respuesta) => setUser(respuesta));
+  }, []);
+
   return (
-    <div className="item-list-container">
-      <h2>{greeting}</h2>
-      <ul>
-        
-      </ul>
-    </div>
+    <>
+      <div className="item-list-container">
+        <h2>{greeting}</h2>
+
+        <ul className="item-list">
+          <li className="item-card" key={user.id}>
+            <img src={user.avatar} alt={user.first_name} />
+            <h4>{`${user.first_name} ${user.last_name}`}</h4>
+            <small>{user.email}</small>
+            <p>$5000</p>
+            <button>Agregar al carrito</button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
-        
 }
 
 export default ItemDetailContainer;
