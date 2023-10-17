04f62c6f482f7a67540d0f21f761cdedc329ada7
abraham leon <115593678+Ephitap@users.noreply.github.com>
2023-10-17 02:54:54 -0300

    Update firestore.js

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore,addDoc,doc,collection,writeBatch,} from "firebase/firestore";
-
+import { getAnalytics } from "firebase/analytics";
 /*cambiar esta shit */
 //-------------------config firebase--------------------------
 const firebaseConfig = {
